1282fb098dd44bd5bff0bc324ec95fc45e148cfe
quyp <1095698022@qq.com>
2023-02-24 17:58:59 +0800

    update

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,8 +1,19 @@
 export default function Layout() {
   return (
     <div className="nav">
-      <div>img</div>
-      <ul>
+      <div className="left">
+        <img src="" />
+        <div>
+          <h4>mia Qu</h4>
+          <h5>a developer</h5>
+          <p>快乐，努力，自由。</p>
+        </div>
+      </div>
+      <div className="right">
+        <p>主题</p>
+        <button>connect me</button>
+      </div>
+      {/* <ul>
         <li>
           <a>blog</a>
         </li>
@@ -15,7 +26,7 @@ export default function Layout() {
         <li>
           <a>关于自己</a>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
