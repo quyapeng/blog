daa394505094b86cb2a2921dc7aedba79640823f
quyp <1095698022@qq.com>
2023-03-03 18:22:48 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Image src={"/images/ban1.png"} width={100} height={100} alt="" />
+        {/* <Image src={"/images/ban1.png"} width={100} height={100} alt="" /> */}
       </main>
     </>
   );
